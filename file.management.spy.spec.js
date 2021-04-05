46f1a560ae418ace087831dc8a2a29fbe0a47d91
diogoo9 <diogo-ti@outlook.com>
2021-04-05 08:09:14 -0300

    using spy

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -1,11 +1,34 @@
 const { expect } = require("chai");
-const { createSandbox, spy } = require("sinon");
+const sinon = require("sinon");
 const fs = require("fs");
 const proxyquire = require("proxyquire");
 const fileManagement = require("./file.management");
 
 describe("File Management", () => {
-  it("true should be true", () => {
-    expect(true).to.be.true;
+
+  afterEach(()=>{
+    sinon.restore();
+  })
+
+  describe("When creating a file,", () => {
+    it("should call writeFileSync", () => {
+      const writeSpy = sinon.spy(fs, "writeFileSync");
+      const fileManagement = proxyquire("./file.management", { fs });
+      fileManagement.createFile("test.txt");
+
+      expect(writeSpy.calledWith("./data/test.txt", "")).to.be.true;
+    });
+
+    it("Should not create a new file if no file name is specified",()=>{
+      const writeSpy = sinon.spy(fs, "writeFileSync");
+      const fileManagement = proxyquire("./file.management", { fs });
+      
+      try {
+        fileManagement.createFile();        
+      } catch (error) {}
+
+      expect(writeSpy.notCalled).to.be.true;
+    });
+
   });
 });
